c89113219550177b0a93820e8c785f5e07a7f534
Ty <74572291+TyNoOutlet@users.noreply.github.com>
2021-07-19 14:05:19 -0400

    Create Sample.lua

---
+++ Sample.lua
@@ -0,0 +1,9 @@
+local yoru = require(7121311219)
+
+yoru.Interpret([[
+
+get function environment has require is print 
+--$above function is basically rewriting the require function$
+require .$howdy$. 
+--$a demo$
+]])
